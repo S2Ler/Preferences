85afddc48e51ee18ed54ed8eaad1fc6135c4fbbc
Alexander Belyavskiy <diejmon@gmail.com>
2019-05-05 22:57:24 +0300

    macOS target

---
+++ Xcode/Preferences (macOS)/Preferences__macOS_.h
@@ -0,0 +1,13 @@
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Preferences__macOS_.
+FOUNDATION_EXPORT double Preferences__macOS_VersionNumber;
+
+//! Project version string for Preferences__macOS_.
+FOUNDATION_EXPORT const unsigned char Preferences__macOS_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Preferences__macOS_/PublicHeader.h>
+
+
